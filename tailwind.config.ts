029a3ea7013ec9ce27b3d3451c6857d6d22e31fc
José Caldas <jrcaldas@yahoo.com.br>
2025-01-26 20:34:17 -0300

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,32 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '.5rem',
+          sm: '1rem',
+        },
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        verde: {
+          200: '#ACEF75',
+          300: '#91EE77',
+          400: '#17E880',
+          700: '#2E482C',
+          800: '#16281F',
+          900: '#0F1C15',
+          950: '#030504',
+        },
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
